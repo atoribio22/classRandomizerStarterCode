9d88a79ceff45f82f23456d3ad8604a633314074
Ashley Toribio <atoribio@aclanyc.com>
2017-05-03 12:10:07 +0000

    stuff

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+    var students = ["Brittany", "Jeanette", "Wilder", "Julio", "Ashley"];
+    var teachers = ["Justin", "Aaron", "Zack", "Julia"];
+
+   
 
 
+    $("#studentButton").click( function(){
+        var random_st = Math.floor(Math.random()*students.length);
+        $("#studentDisplay").append(students[random_st]);
+    });
 
+    $("#teacherButton").click( function(){
+        var random_te = Math.floor(Math.random()*teachers.length);
+        $("#teacherDisplay").append(teachers[random_te]);
+    });
 });
